8377ff5f8791314add65d627d3e392e6f25a0f0b
Yuss76A <yuse_91yus@hotmail.com>
2025-03-30 23:33:22 +0200

    Create a auth_backend.py file and add class backend code on the file

--- backendapp/settings.py
+++ backendapp/settings.py
@@ -137,3 +137,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'carbooking.User'
+AUTHENTICATION_BACKENDS = ['carbooking.auth_backend.EmailBackend', 'django.contrib.auth.backends.ModelBackend']
