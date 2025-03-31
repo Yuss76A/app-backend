5ca12ca94f2d9d44bc7c046ec82148d35f7366ad
Yuss76A <yuse_91yus@hotmail.com>
2025-04-01 00:21:22 +0200

    Added reviews path url in the main directory urls file

--- backendapp/urls.py
+++ backendapp/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('carbooking/', include('carbooking.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('reviews/', include('reviews.urls')),
     path('', api_root, name='api-root'),
 ]
