8a9e4d2043b94c2166769606ba6466da0e4fa3f6
Yuss76A <yuse_91yus@hotmail.com>
2025-03-31 22:54:48 +0200

    Register User, Car, BookedDate, and CarImage models in the admin site

--- carbooking/admin.py
+++ carbooking/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Car
+from .models import User, Car, BookedDate, CarImage
 
 
+admin.site.register(User)
 admin.site.register(Car)
+admin.site.register(BookedDate)
+admin.site.register(CarImage)
